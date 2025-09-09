a8590fe885058c86078cee31eb2bcfd288e75f44
NKM <scr08212@gmail.com>
2025-09-09 22:06:23 +0900

    김영한 mvc 1편 - 13. HttpServletRequest - 기본 사용법

---
+++ servlet/src/main/java/nkm/study/servlet/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,36 @@
+package nkm.study.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
+public class RequestHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        printStartLine(request);
+        printHeaderNames(request);
+    }
+
+    private static void printHeaderNames(HttpServletRequest request) {
+        request.getHeaderNames()
+                .asIterator()
+                .forEachRemaining(System.out::println);
+    }
+
+    private static void printStartLine(HttpServletRequest request) {
+        System.out.println("--- REQUEST LINE - BEGIN ---");
+        System.out.println("requset.getMethod() = " + request.getMethod());
+        System.out.println("request.getProtocol() = " + request.getProtocol());
+        System.out.println("request.getScheme() = " + request.getScheme());
+        System.out.println("request.getRequestURL() = " + request.getRequestURL());
+        System.out.println("request.getRequestURI() = " + request.getRequestURI());
+        System.out.println("request.getQueryString() = " + request.getQueryString());
+        System.out.println("request.isSecure() = " + request.isSecure());
+        System.out.println("--- REQUEST LINE - END ---");
+    }
+}
