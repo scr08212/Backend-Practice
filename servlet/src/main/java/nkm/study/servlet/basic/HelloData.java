57835d31c403fefb642ab03bf5c9da97f98ca828
NKM <scr08212@gmail.com>
2025-09-10 14:05:46 +0900

    김영한 mvvc 1편 - 18. HTTP 요청 데이터 - API 메시지 바디 - JSON

---
+++ servlet/src/main/java/nkm/study/servlet/basic/HelloData.java
@@ -0,0 +1,12 @@
+package nkm.study.servlet.basic;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloData {
+    private String username;
+    private String age;
+}
