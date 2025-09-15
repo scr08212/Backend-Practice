b68c1fc496db58373aef250c89c7be3d52022c05
NKM <scr08212@gmail.com>
2025-09-15 23:47:34 +0900

    김영한 MVC 1편 - 35. 유연한 컨트롤러1 - v5

---
+++ servlet/src/main/java/nkm/study/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdpater.java
@@ -0,0 +1,35 @@
+package nkm.study.servlet.web.frontcontroller.v5.adapter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import nkm.study.servlet.web.frontcontroller.ModelView;
+import nkm.study.servlet.web.frontcontroller.v3.ControllerV3;
+import nkm.study.servlet.web.frontcontroller.v4.ControllerV4;
+import nkm.study.servlet.web.frontcontroller.v5.MyHandlerAdapter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerV3HandlerAdpater implements MyHandlerAdapter {
+    @Override
+    public boolean support(Object handler) {
+        return (handler instanceof ControllerV3);
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
+
+        ControllerV3 controller = (ControllerV3)handler;
+
+        return controller.process(createParamMap(request));
+    }
+
+    private static Map<String, String> createParamMap(HttpServletRequest req) {
+        Map<String, String> paramMap = new HashMap<>();
+        req.getParameterNames().asIterator()
+                .forEachRemaining(paramName-> paramMap.put(paramName, req.getParameter(paramName)));
+        return paramMap;
+    }
+}
