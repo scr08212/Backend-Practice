ee8d80613eb76608e9c21695b56a4ddbb39c1e6b
NKM <scr08212@gmail.com>
2025-09-15 23:56:38 +0900

    김영한 MVC 1편 - 36. 유연한 컨트롤러2 - v5

--- servlet/src/main/java/nkm/study/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ servlet/src/main/java/nkm/study/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -10,6 +10,9 @@ import nkm.study.servlet.web.frontcontroller.MyView;
 import nkm.study.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import nkm.study.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import nkm.study.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import nkm.study.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import nkm.study.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import nkm.study.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import nkm.study.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdpater;
 
 import java.io.IOException;
@@ -33,10 +36,15 @@ public class FrontControllerServletV5 extends HttpServlet {
         handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+
+        handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
     }
 
     private void initHandlerAdapters() {
         handlerAdapters.add(new ControllerV3HandlerAdpater());
+        handlerAdapters.add(new ControllerV4HandlerAdpater());
     }
 
     @Override
