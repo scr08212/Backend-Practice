2fda971058e31a94f8a44b7874c265f49a69949f
NKM <scr08212@gmail.com>
2025-09-15 20:42:00 +0900

    김영한 MVC 1편 - 34. 단순하고 실용적인 컨트롤러 - v4

---
+++ servlet/src/main/java/nkm/study/servlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
@@ -0,0 +1,21 @@
+package nkm.study.servlet.web.frontcontroller.v4.controller;
+
+import nkm.study.servlet.domain.member.Member;
+import nkm.study.servlet.domain.member.MemberRepository;
+import nkm.study.servlet.web.frontcontroller.ModelView;
+import nkm.study.servlet.web.frontcontroller.v4.ControllerV4;
+
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV4 implements ControllerV4 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        List<Member> members = memberRepository.findAll();
+        model.put("members", members);
+        return "members";
+    }
+}
