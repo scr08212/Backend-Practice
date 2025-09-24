6333c734c964611cba79be12171b57aba75c57a5
NKM <scr08212@gmail.com>
2025-09-24 23:38:12 +0900

    김영한 MVC 1편 - 70. RedirectAttributes

--- item-service/src/main/java/nkm/study/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/nkm/study/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import nkm.study.itemservice.domain.item.ItemRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -55,9 +56,12 @@ public class BasicItemController {
     }
 
     @PostMapping("/add")
-    public String addItemV2(@ModelAttribute("item") Item item){
-        itemRepository.save(item);
-        return "redirect:/basic/items";
+    public String addItemV2(@ModelAttribute("item") Item item, RedirectAttributes redirectAttributes){
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
