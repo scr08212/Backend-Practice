d37dd98272c6f763cf653efda614e3a70ecff13d
NKM <scr08212@gmail.com>
2025-09-24 23:09:16 +0900

    김영한 MVC 1편 - 66. 상품 등록 폼

--- item-service/src/main/java/nkm/study/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/nkm/study/itemservice/web/basic/BasicItemController.java
@@ -6,9 +6,7 @@ import nkm.study.itemservice.domain.item.Item;
 import nkm.study.itemservice.domain.item.ItemRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,4 +37,14 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(@ModelAttribute("item") Item item){
+        itemRepository.save(item);
+        return "redirect:/basic/items";
+    }
 }
