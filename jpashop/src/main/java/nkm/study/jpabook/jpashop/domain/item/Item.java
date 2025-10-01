b47356de098a93060ff8a122caea93fcbe234459
NKM <scr08212@gmail.com>
2025-10-02 00:18:15 +0900

    김영한 JPA 활용1 - 12. 엔티티 클래스 개발2

--- jpashop/src/main/java/nkm/study/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/nkm/study/jpabook/jpashop/domain/item/Item.java
@@ -3,6 +3,9 @@ package nkm.study.jpabook.jpashop.domain.item;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import nkm.study.jpabook.jpashop.domain.Category;
+
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -20,4 +23,7 @@ public abstract class Item {
     private int price;
 
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories;
 }
