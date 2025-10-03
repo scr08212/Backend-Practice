add1ea3cd53def0df589e0bccb3b1284534b3a49
NKM <scr08212@gmail.com>
2025-10-03 13:32:12 +0900

    김영한 JPA 활용1 - 18. 회원 기능 테스트

---
+++ jpashop/src/test/java/nkm/study/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,45 @@
+package nkm.study.jpabook.jpashop.service;
+
+import nkm.study.jpabook.jpashop.domain.Member;
+import nkm.study.jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    private MemberService memberService;
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입(){
+        Member member = new Member();
+        member.setName("kim");
+
+        Long savedId = memberService.join(member);
+
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test
+    public void 중복_회원_예외(){
+        Member member = new Member();
+        member.setName("kim");
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        memberService.join(member);
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+    }
+}
