76cc54d1d2996d2a69f5010fd5afa74935b001c6
NKM <scr08212@gmail.com>
2025-10-03 14:29:42 +0900

    김영한 JPA 활용1 - 23. 주문 리포지토리 개발

---
+++ jpashop/src/main/java/nkm/study/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,24 @@
+package nkm.study.jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import nkm.study.jpabook.jpashop.domain.Order;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(int orderId) {
+        return em.find(Order.class, orderId);
+    }
+
+//    public List<Order> findAll(OrderSearch orderSearch) {}
+}
