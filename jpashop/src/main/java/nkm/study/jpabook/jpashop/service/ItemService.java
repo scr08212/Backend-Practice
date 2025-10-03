371712093573f01da9a19e3785c042cebb010e19
NKM <scr08212@gmail.com>
2025-10-03 14:00:18 +0900

    김영한 JPA 활용1 - 21. 상품 서비스 개발

---
+++ jpashop/src/main/java/nkm/study/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,29 @@
+package nkm.study.jpabook.jpashop.service;
+
+import lombok.RequiredArgsConstructor;
+import nkm.study.jpabook.jpashop.domain.item.Item;
+import nkm.study.jpabook.jpashop.repository.ItemRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly=true)
+@RequiredArgsConstructor
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void save(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findAll() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long id) {
+        return itemRepository.findOne(id);
+    }
+}
