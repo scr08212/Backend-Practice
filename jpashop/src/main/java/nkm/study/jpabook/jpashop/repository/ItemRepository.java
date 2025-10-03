b375b405c8c574bc75023f9bf622a48bbd993151
NKM <scr08212@gmail.com>
2025-10-03 13:57:56 +0900

    김영한 JPA 활용1 - 20. 상품 리포지토리 개발

---
+++ jpashop/src/main/java/nkm/study/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,33 @@
+package nkm.study.jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import nkm.study.jpabook.jpashop.domain.item.Item;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if(item.getId()==null){
+            em.persist(item);
+        }
+        else {
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
